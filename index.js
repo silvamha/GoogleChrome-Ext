791fd9e827648e056e9c725d128fda195d8c976b
Ed <silva.email@yahoo.com>
2024-09-21 13:58:06 -0500

    Fixed loop

--- index.js
+++ index.js
@@ -1,4 +1,32 @@
-const inputEl =document.querySelector('#input-el')
-const inputBtn =document.querySelector('#input-btn')
+const inputEl = document.querySelector("#input-el");
+const inputBtn = document.querySelector("#input-btn");
+const divUlContainer = document.createElement("div");
+document.querySelector("body").append(divUlContainer);
+divUlContainer.setAttribute("id", "div-ul-container-el");
+const uLEl = document.createElement("ul");
+uLEl.setAttribute("id", "ul-el");
+document.querySelector("#div-ul-container-el").append(uLEl);
 
-console.log(inputEl, inputBtn)
+let myLeads = [];
+
+// console.log(myLeads);
+// console.log(displayLeads);
+
+inputBtn.addEventListener("click", () => {
+  let inputValue = inputEl.value;
+  myLeads.push(inputValue);
+
+  const liEl = document.createElement("li");
+
+  liEl.textContent = inputValue;
+  uLEl.append(liEl);
+  uLEl.appendChild(liEl)
+  console.log(myLeads);
+//   console.log(liEl.textContent);
+
+  clearField();
+});
+
+let clearField = () => {
+  inputEl.value = "";
+};
