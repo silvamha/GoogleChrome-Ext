c2bab9e50979c9ac0ff10499d9a8dfc7a156ff3c
Ed <silva.email@yahoo.com>
2024-09-22 04:56:10 -0500

    localStorage Setup

--- index.js
+++ index.js
@@ -9,24 +9,44 @@ document.querySelector("#div-ul-container-el").append(uLEl);
 
 let myLeads = [];
 
+// myLeads = JSON.parse(myLeads);
+
+// myLeads.push("www.rock.com")
+
+// myLeads = JSON.stringify(myLeads);
+
+// console.log(typeof myLeads)
+
+let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+
+console.log(leadsFromLocalStorage)
+
 inputBtn.addEventListener("click", () => {
   let inputValue = inputEl.value;
   myLeads.push(inputValue);
+  clearField();
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  
+  console.log( localStorage.getItem("myLeads") )
+});
 
-  const liEl = document.createElement("li");
-  liEl.textContent = inputValue;
+const renderLeads = () =>{
+const liEl = document.createElement("li");
+  //   liEl.textContent = inputValue;
   liEl.setAttribute("class", "li-el");
 
   const aEl = document.createElement("a");
-  aEl.href = "inputValue";
+  aEl.href = inputValue;
   aEl.textContent = inputValue;
+  aEl.target = "_blank";
+
   liEl.appendChild(aEl);
   uLEl.appendChild(liEl);
-  console.log(myLeads);
 
-  clearField();
   console.log(myLeads);
-});
+  console.log(aEl.href);
+  console.log(uLEl);
+}
 
 let clearField = () => {
   inputEl.value = "";
